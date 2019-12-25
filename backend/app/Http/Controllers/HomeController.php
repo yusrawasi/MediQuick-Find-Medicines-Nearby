b0d0e9ec65492c9baa496b4ee25ceccf709b92de
usmansabir98 <usmansabir98@hotmail.com>
2019-12-26 01:06:20 +0500

    make project dockerized

---
+++ backend/app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * Show the profile for the given user.
+     *
+     * @param  int  $id
+     * @return View
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
+}
